545b37934a5c02f98eeb739792bcf4b708e8833d
Nasim <mdnasimnasim2007@gmail.com>
2025-09-26 00:59:17 +0600

    f

--- lib/untitled.dart
+++ lib/untitled.dart
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 class nasim {
-  String name ="nasim";
-=======
+  String name = "nasim";
+}
 void main(){
   print("nasim");
   print("josim");
   var age =10;
   print("sdfgh");
->>>>>>> parent of a1565d2 (mnsj)
+
 }
